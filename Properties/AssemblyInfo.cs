55db0c739c659d7f5288ae7cef193383d779988b
KAMADA Ken'ichi <kamada@nanohz.org>
2016-03-13 10:01:08 +0900

    Update properties.
    - Do not prefer 32-bit mode.
    - Update the assembly name and title.
    - Update the copyright.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lurkingwind")]
+[assembly: AssemblyTitle("Lurking Window Detector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lurkingwind")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Lurking Window Detector")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 KAMADA Ken'ichi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
