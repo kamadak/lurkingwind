130e5bbee925f22aca5ead53a9e3a2c57ce4506f
KAMADA Ken'ichi <kamada@nanohz.org>
2016-03-17 21:16:40 +0900

    Change the title to "Options".

--- OptionsForm.Designer.cs
+++ OptionsForm.Designer.cs
@@ -75,7 +75,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "OptionsForm";
-            this.Text = "OptionsForm";
+            this.Text = "Options";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
